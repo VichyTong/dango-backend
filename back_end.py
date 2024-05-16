9c0a56ffa33eb4750e081ed6af1446751729892f
Weixi Tong <weixitong@hust.edu.cn>
2024-05-16 14:54:31 +0000

    [fix] Modify should provide the client ID

--- back_end.py
+++ back_end.py
@@ -60,23 +60,25 @@ async def upload_file(client_id: str = Form(...), file: UploadFile = File(...)):
     )
 
 
-@app.post("/modify/{sheet_id}")
-async def modify_file(sheet_id: str, file: UploadFile = File(...)):
+@app.post("/modify/")
+async def modify_file(
+    client_id: str = Form(...), sheet_id: str = Form(...), file: UploadFile = File(...)
+):
     if not file.filename.endswith(".csv"):
         raise HTTPException(status_code=400, detail="Unsupported file type")
-    file_path = os.path.join(UPLOAD_FOLDER, sheet_id)
+    file_path = os.path.join(UPLOAD_FOLDER, f"{client_id}_{sheet_id}")
 
     # Replace the file
     try:
-        with open(file_path, "wb") as buffer:
+        async with aiofiles.open(file_path, "wb") as buffer:
             while data := await file.read(1024):
-                buffer.write(data)
+                await buffer.write(data)
     except IOError as e:
         raise HTTPException(status_code=500, detail=f"File modification failed: {e}")
 
     return JSONResponse(
         status_code=200,
-        content={"message": "File modified successfully", "sheet_id": sheet_id},
+        content={"message": f"{sheed_id} modified successfully"},
     )
 
 
