7df37f76f5c57537e8f5353c4ef660040caf6653
Weixi Tong <weixitong@hust.edu.cn>
2024-05-20 14:18:35 +0000

    [feat] Return natural language of DSL

--- utils/compile.py
+++ utils/compile.py
@@ -35,6 +35,82 @@ def create_user_prompt(history):
     return user_prompt
 
 
+def transfer_to_NL(dsl):
+    if dsl["function_name"] == "drop":
+        table = dsl["arguments"][0]
+        label = dsl["arguments"][1]
+        axis = dsl["arguments"][2]
+        if axis == 0 or axis == "index":
+            return f"Drop the row with {label} in {table}."
+        elif axis == 1 or axis == "column":
+            return f"Drop the column with {label} in {table}."
+        else:
+            return "Invalid function"
+    elif dsl["function_name"] == "move":
+        table = dsl["arguments"][0]
+        label = dsl["arguments"][1]
+        target_table = dsl["arguments"][2]
+        target_position = dsl["arguments"][3]
+        axis = dsl["arguments"][4]
+        if axis == 0 or axis == "index":
+            return f"Move the row with {label} in {table} to {target_table} at position {target_position}."
+        elif axis == 1 or axis == "column":
+            return f"Move the column with {label} in {table} to {target_table} at position {target_position}."
+        else:
+            return "Invalid function"
+    elif dsl["function_name"] == "copy":
+        table = dsl["arguments"][0]
+        label = dsl["arguments"][1]
+        target_table = dsl["arguments"][2]
+        target_position = dsl["arguments"][3]
+        axis = dsl["arguments"][4]
+        if axis == 0 or axis == "index":
+            return f"Copy the row with {label} in {table} to {target_table} at position {target_position}."
+        elif axis == 1 or axis == "column":
+            return f"Copy the column with {label} in {table} to {target_table} at position {target_position}."
+        else:
+            return "Invalid function"
+    elif dsl["function_name"] == "merge":
+        table = dsl["arguments"][0]
+        label_1 = dsl["arguments"][1]
+        label_2 = dsl["arguments"][2]
+        glue = dsl["arguments"][3]
+        new_label = dsl["arguments"][4]
+        axis = dsl["arguments"][5]
+        if axis == 0 or axis == "index":
+            return f"Merge the row with {label_1} and {label_2} in {table} with \"{glue}\" as {new_label}."
+        elif axis == 1 or axis == "column":
+            return f"Merge the column with {label_1} and {label_2} in {table} with \"{glue}\" as {new_label}."
+        else:
+            return "Invalid function"
+    elif dsl["function_name"] == "split":
+        table = dsl["arguments"][0]
+        label = dsl["arguments"][1]
+        delimiter = dsl["arguments"][2]
+        new_labels = dsl["arguments"][3]
+        axis = dsl["arguments"][4]
+        if axis == 0 or axis == "index":
+            return f"Split the row with {label} in {table} by \"{delimiter}\" as {new_labels}."
+        elif axis == 1 or axis == "column":
+            return f"Split the column with {label} in {table} by \"{delimiter}\" as {new_labels}."
+        else:
+            return "Invalid function"
+    elif dsl["function_name"] == "transpose":
+        table = dsl["arguments"][0]
+        return f"Transpose {table}."
+    elif dsl["function_name"] == "aggregate":
+        table = dsl["arguments"][0]
+        label = dsl["arguments"][1]
+        operation = dsl["arguments"][2]
+        axis = dsl["arguments"][3]
+        if axis == 0 or axis == "index":
+            return f"Aggregate the row with {label} in {table} with {operation}."
+        elif axis == 1 or axis == "column":
+            return f"Aggregate the column with {label} in {table} with {operation}."
+        else:
+            return "Invalid function"
+
+
 def dsl_compile(client_id: str) -> str:
     history = get_client(client_id).history
     user_prompt = create_user_prompt(history)
@@ -42,8 +118,9 @@ def dsl_compile(client_id: str) -> str:
     _, client = create_client()
     client.append_system_message(system_prompt)
     client.append_user_message(user_prompt)
-    print(client.history)
     response = client.generate_chat_completion()
-    print(response)
-    dsl = json.loads(response)
-    return dsl
+    dsls = json.loads(response)
+    for dsl in dsls:
+        dsl["naturl_language"] = transfer_to_NL(dsl)
+    print(dsls)
+    return dsls
