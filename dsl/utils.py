6461bdc5d06ffd5fd34dbcbb74177535365734b3
Weixi Tong <weixitong@hust.edu.cn>
2024-07-22 13:21:47 +0000

    [fix] Fix fill function

--- dsl/utils.py
+++ dsl/utils.py
@@ -673,35 +673,44 @@ def fill(table, method, column=None):
     - column: The column to fill missing values in. If None, missing values in all columns will be filled.
     """
     df = table.copy()
+    if not isinstance(column, list):
+        columns = [column]
+    else:
+        columns = None
 
     if method == "mean":
-        if column:
-            df[column].fillna(df[column].mean(), inplace=True)
+        if columns:
+            for column in columns:
+                df[column].fillna(df[column].mean(), inplace=True)
         else:
             for col in df.columns:
                 if df[col].dtype != "O":  # Skip non-numeric columns
                     df[col].fillna(df[col].mean(), inplace=True)
     elif method == "median":
-        if column:
-            df[column].fillna(df[column].median(), inplace=True)
+        if columns:
+            for column in columns:
+                df[column].fillna(df[column].median(), inplace=True)
         else:
             for col in df.columns:
                 if df[col].dtype != "O":  # Skip non-numeric columns
                     df[col].fillna(df[col].median(), inplace=True)
     elif method == "mode":
-        if column:
-            df[column].fillna(df[column].mode()[0], inplace=True)
+        if columns:
+            for column in columns:
+                df[column].fillna(df[column].mode()[0], inplace=True)
         else:
             for col in df.columns:
                 df[col].fillna(df[col].mode()[0], inplace=True)
     elif method in ["ffill", "bfill"]:
-        if column:
-            df[column].fillna(method=method, inplace=True)
+        if columns:
+            for column in columns:
+                df[column].fillna(method=method, inplace=True)
         else:
             df.fillna(method=method, inplace=True)
     elif method == "interpolate":
-        if column:
-            df[column].interpolate(inplace=True)
+        if columns:
+            for column in columns:
+                df[column].interpolate(inplace=True)
         else:
             df.interpolate(inplace=True)
     else:
