af26c13285f593517f8543e700f36e4dc96d8733
Weihao Chen <ferrans.chen@gmail.com>
2024-08-01 13:05:32 -0400

    [fix] change transfer to nl to normal text without annotation

--- utils/synthesize.py
+++ utils/synthesize.py
@@ -62,216 +62,183 @@ init_prompt()
 
 
 def transfer_to_NL(dsl):
-    # notations:
-    # %[given table(s)] -> table
-    # $[{index}] -> row or column name
-    # &[{glue}] -> glue
-    # *[{how}] -> how
-    # #[{index}] -> index
-
     if dsl["function_name"] == "create_table":
-        table = dsl["arguments"][0]
         row_number = dsl["arguments"][1]
         column_number = dsl["arguments"][2]
-        return f"Create a table %[given table(s)] with #[{row_number}] rows and #[{column_number}] columns"
+        return f"Create a table with {row_number} rows and {column_number} columns"
     elif dsl["function_name"] == "delete_table":
         table = dsl["arguments"][0]
-        return f"Delete the table %[{table}]"
+        return f"Delete the table {table}"
     elif dsl["function_name"] == "insert":
-        table = dsl["arguments"][0]
         index = dsl["arguments"][1]
-        index_name = dsl["arguments"][2]
         axis = dsl["arguments"][3]
         if axis == 0 or axis == "index" or axis == "0":
-            return f"Insert a row at position #[{index}] in the %[given table(s)]"
+            return f"Insert a row at position {index} in the given table(s)"
         elif axis == 1 or axis == "columns" or axis == "1":
-            return f"Insert a column at position #[{index}] in the %[given table(s)]"
+            return f"Insert a column at position {index} in the given table(s)"
         else:
             return "Invalid function"
     elif dsl["function_name"] == "drop":
-        table = dsl["arguments"][0]
         label = dsl["arguments"][1]
         axis = dsl["arguments"][2]
         if axis == 0 or axis == "index" or axis == "0":
-            return f"Drop the row #[{label}] in %[given table(s)]"
+            return f"Drop the row {label} in the given table(s)"
         elif axis == 1 or axis == "columns" or axis == "1":
-            return f"Drop the column #[{label}] in %[given table(s)]"
+            return f"Drop the column {label} in the given table(s)"
         else:
             return "Invalid function"
     elif dsl["function_name"] == "assign":
-        table = dsl["arguments"][0]
         start_row_index = dsl["arguments"][1]
         end_row_index = dsl["arguments"][2]
         start_column_index = dsl["arguments"][3]
         end_column_index = dsl["arguments"][4]
         values = dsl["arguments"][5]
         string_values = json.dumps(values)
-        return f"Assign the values @[{string_values}] in %[given table(s)]"
+        return f"Assign the values {string_values} in the given table(s)"
     elif dsl["function_name"] == "move":
-        origin_table = dsl["arguments"][0]
         origin_index = dsl["arguments"][1]
-        target_table = dsl["arguments"][2]
         target_index = dsl["arguments"][3]
         axis = dsl["arguments"][4]
         if axis == 0 or axis == "index" or axis == "0":
-            return f"Move the row #[{origin_index}] to row #[{target_index}] in the %[given table(s)]"
+            return f"Move the row {origin_index} to row {target_index} in the given table(s)"
         elif axis == 1 or axis == "columns" or axis == "1":
-            return f"Move the column #[{origin_index}] to column #[{target_index}] in the %[given table(s)]"
+            return f"Move the column {origin_index} to column {target_index} in the given table(s)"
         else:
             return "Invalid function"
     elif dsl["function_name"] == "copy":
-        origin_table = dsl["arguments"][0]
         origin_index = dsl["arguments"][1]
-        target_table = dsl["arguments"][2]
         target_index = dsl["arguments"][3]
-        target_label_name = dsl["arguments"][4]
         axis = dsl["arguments"][5]
         if axis == 0 or axis == "index" or axis == "0":
-            return f"Copy the row #[{origin_index}] to row #[{target_index}] in the %[given table(s)]"
+            return f"Copy the row {origin_index} to row {target_index} in the given table(s)"
         elif axis == 1 or axis == "columns" or axis == "1":
-            return f"Copy the column #[{origin_index}] to column #[{target_index}] in the %[given table(s)]"
+            return f"Copy the column {origin_index} to column {target_index} in the given table(s)"
         else:
             return "Invalid function"
     elif dsl["function_name"] == "swap":
-        table_a = dsl["arguments"][0]
         label_a = dsl["arguments"][1]
-        table_b = dsl["arguments"][2]
         label_b = dsl["arguments"][3]
         axis = dsl["arguments"][4]
         if axis == 0 or axis == "index" or axis == "0":
-            return f"Swap the row $[{label_a}] and the row $[{label_b}] in the %[given table(s)]"
+            return f"Swap the row {label_a} and the row {label_b} in the given table(s)"
         elif axis == 1 or axis == "columns" or axis == "1":
-            return f"Swap the column $[{label_a}] and the column $[{label_b}] in the %[given table(s)]"
+            return f"Swap the column {label_a} and the column {label_b} in the given table(s)"
         else:
             return "Invalid function"
     elif dsl["function_name"] == "merge":
-        table_a = dsl["arguments"][0]
         table_b = dsl["arguments"][1]
-        how = dsl["arguments"][2]
         on = dsl["arguments"][3]
         axis = dsl["arguments"][4]
         if axis == 0 or axis == "index" or axis == "0":
-            return f"Merge the %[given table(s)] with the table $[{table_b}]"
+            return f"Merge the given table(s) with the table {table_b}"
         elif axis == 1 or axis == "columns" or axis == "1":
             if on is not None:
-                return f"Merge the %[given table(s)] with the table $[{table_b}] based on the values in the column $[{on}]"
-            elif left_on is not None and right_on is not None:
-                return f"Merge the %[given table(s)] with the table $[{table_b}] based on the values in the column $[{left_on}] and $[{right_on}]"
+                return f"Merge the given table(s) with the table {table_b} based on the values in the column {on}"
             else:
                 return "Invalid function"
         else:
             return "Invalid function"
     elif dsl["function_name"] == "concatenate":
-        table = dsl["arguments"][0]
         label_a = dsl["arguments"][1]
         label_b = dsl["arguments"][2]
         glue = dsl["arguments"][3]
-        new_label = dsl["arguments"][4]
         axis = dsl["arguments"][5]
         if axis == 0 or axis == "index" or axis == "0":
-            return f"Concatenate the rows $[{label_a}] and $[{label_b}] in %[given table(s)] with the glue &[{glue}]"
+            return f"Concatenate the rows {label_a} and {label_b} in the given table(s) with the glue {glue}"
         elif axis == 1 or axis == "columns" or axis == "1":
-            return f"Concatenate the columns $[{label_a}] and $[{label_b}] in %[given table(s)] with the glue &[{glue}]"
+            return f"Concatenate the columns {label_a} and {label_b} in the given table(s) with the glue {glue}"
         else:
             return "Invalid function"
     elif dsl["function_name"] == "split":
-        table = dsl["arguments"][0]
         label = dsl["arguments"][1]
         delimiter = dsl["arguments"][2]
         axis = dsl["arguments"][3]
         if axis == 0 or axis == "index" or axis == "0":
-            return f"Split the values in the row $[{label}] in %[given table(s)] with the delimiter &[{delimiter}]"
+            return f"Split the values in the row {label} in the given table(s) with the delimiter {delimiter}"
         elif axis == 1 or axis == "columns" or axis == "1":
-            return f"Split the values in the column $[{label}] in %[given table(s)] with the delimiter &[{delimiter}]"
-    elif dsl["function_name"] == "transpose":
-        table = dsl["arguments"][0]
-        return f"Transpose %[given table(s)]"
-    elif dsl["function_name"] == "aggregate":
-        table = dsl["arguments"][0]
-        functions = dsl["arguments"][1]
-        axis = dsl["arguments"][2]
-        if axis == 0 or axis == "index" or axis == "0":
-            return f"Aggregate %[given table(s)] with the functions $[{functions}]"
-        elif axis == 1 or axis == "columns" or axis == "1":
-            return f"Aggregate %[given table(s)] with the functions $[{functions}]"
+            return f"Split the values in the column {label} in the given table(s) with the delimiter {delimiter}"
         else:
             return "Invalid function"
+    elif dsl["function_name"] == "transpose":
+        return f"Transpose the given table(s)"
+    elif dsl["function_name"] == "aggregate":
+        functions = dsl["arguments"][1]
+        axis = dsl["arguments"][2]
+        return f"Aggregate the given table(s) with the functions {functions}"
     elif dsl["function_name"] == "test":
-        table = dsl["arguments"][0]
         label_a = dsl["arguments"][1]
         label_b = dsl["arguments"][2]
         strategy = dsl["arguments"][3]
         axis = dsl["arguments"][4]
         if axis == 0 or axis == "index" or axis == "0":
-            return f"Test the rows $[{label_a}] and $[{label_b}] in %[given table(s)] with the strategy *[{strategy}]"
+            return f"Test the rows {label_a} and {label_b} in the given table(s) with the strategy {strategy}"
         elif axis == 1 or axis == "columns" or axis == "1":
-            return f"Test the columns $[{label_a}] and $[{label_b}] in %[given table(s)] with the strategy *[{strategy}]"
+            return f"Test the columns {label_a} and {label_b} in the given table(s) with the strategy {strategy}"
         else:
             return "Invalid function"
     elif dsl["function_name"] == "rearrange":
-        table = dsl["arguments"][0]
         by_values = dsl["arguments"][1]
         by_array = dsl["arguments"][2]
         axis = dsl["arguments"][3]
         if by_values is not None:
             if axis == 0 or axis == "index" or axis == "0":
-                return f"Rearrange the rows in %[given table(s)] based on the values in the row $[{by_values}]"
+                return f"Rearrange the rows in the given table(s) based on the values in the row {by_values}"
             elif axis == 1 or axis == "columns" or axis == "1":
-                return f"Rearrange the columns in %[given table(s)] based on the values in the column $[{by_values}]"
+                return f"Rearrange the columns in the given table(s) based on the values in the column {by_values}"
             else:
                 return "Invalid function"
         elif by_array is not None:
             if axis == 0 or axis == "index" or axis == "0":
-                return f"Rearrange the rows in %[given table(s)] based on the order of the rows $[{by_array}]"
+                return f"Rearrange the rows in the given table(s) based on the order of the rows {by_array}"
             elif axis == 1 or axis == "columns" or axis == "1":
-                return f"Rearrange the columns in %[given table(s)] based on the order of the columns $[{by_array}]"
+                return f"Rearrange the columns in the given table(s) based on the order of the columns {by_array}"
             else:
                 return "Invalid function"
         else:
             return "Either by_values or by_array must be provided"
     elif dsl["function_name"] == "format":
-        table = dsl["arguments"][0]
         label = dsl["arguments"][1]
         pattern = dsl["arguments"][2]
         replace_with = dsl["arguments"][3]
         axis = dsl["arguments"][4]
         if axis == 0 or axis == "index" or axis == "0":
-            return f"Format the values in the row $[{label}] in %[given table(s)] with the pattern *[{pattern}] and replace them with *[{replace_with}]"
+            return f"Format the values in the row {label} in the given table(s) with the pattern {pattern} and replace them with {replace_with}"
         elif axis == 1 or axis == "columns" or axis == "1":
-            return f"Format the values in the column $[{label}] in %[given table(s)] with the pattern *[{pattern}] and replace them with *[{replace_with}]"
+            return f"Format the values in the column {label} in the given table(s) with the pattern {pattern} and replace them with {replace_with}"
+        else:
+            return "Invalid function"
     elif dsl["function_name"] == "divide":
-        table = dsl["arguments"][0]
         by = dsl["arguments"][1]
         axis = dsl["arguments"][2]
         if axis == 0 or axis == "index" or axis == "0":
-            return f"Divide the %[given table(s)] by the values in the row $[{by}]"
+            return f"Divide the given table(s) by the values in the row {by}"
         elif axis == 1 or axis == "columns" or axis == "1":
-            return f"Divide the %[given table(s)] by the values in the column $[{by}]"
+            return f"Divide the given table(s) by the values in the column {by}"
+        else:
+            return "Invalid function"
     elif dsl["function_name"] == "fill":
-        table = dsl["arguments"][0]
         method = dsl["arguments"][1]
         column = dsl["arguments"][2]
-        return f"Fill the missing values in the column $[{column}] in %[given table(s)] with the method *[{method}]"
+        return f"Fill the missing values in the column {column} in the given table(s) with the method {method}"
     elif dsl["function_name"] == "pivot_table":
-        table = dsl["arguments"][0]
         index = dsl["arguments"][1]
         columns = dsl["arguments"][2]
         values = dsl["arguments"][3]
         aggfunc = dsl["arguments"][4]
-        return f"Create a pivot table based on the %[given table(s)] with the index $[{index}], columns $[{columns}], values $[{values}] and the aggregation function *[{aggfunc}]"
+        return f"Create a pivot table in the given table(s) with the index {index}, columns {columns}, values {values}, and the aggregation function {aggfunc}"
     elif dsl["function_name"] == "sub_table":
-        table = dsl["arguments"][0]
         label_list = dsl["arguments"][1]
-        new_name = dsl["arguments"][2]
         axis = dsl["arguments"][3]
         if axis == 0 or axis == "index" or axis == "0":
-            return f"Create a sub-table %[given table(s)] with the rows $[{label_list}]"
+            return f"Create a sub-table in the given table(s) with the rows {label_list}"
         elif axis == 1 or axis == "columns" or axis == "1":
-            return f"Create a sub-table %[given table(s)] with the columns $[{label_list}]"
+            return f"Create a sub-table in the given table(s) with the columns {label_list}"
         else:
             return "Invalid function"
     else:
         return "Invalid function"
+
+
 
 
 def get_summarization(client_id, history):
